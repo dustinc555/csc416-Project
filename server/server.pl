7de59de5ba2bb84951601a5dc0dca95193e6dde6
dustin <dustin.cook@mines.sdsmt.edu>
2018-11-13 19:12:28 -0700

    docker ready: server running on port 50000

---
+++ server/server.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+
+use strict; use warnings;
+
+package SimpleServer;
+
+use LWP::Simple;
+use base qw(Net::Server);
+
+# perl v5.28.0
+
+
+sub process_request
+{
+
+    while (<STDIN>) 
+    {    
+        s/[\r\n]+$//;   
+        sleep 5; # dont get black listed
+        
+	my $symbol = $_; # get command
+
+	#my $url = "http://www.nasdaq.com/symbol/$symbol/historical";
+        
+        print "hello  $symbol";
+        
+	#my $html = get("$url"); # get data, die if failure
+	#die "Failed to get page!" unless defined $html;
+
+	#my $stream = HTML::TokeParser->new(\$content);
+	
+
+        # write data to file
+	# $symbol .= ".html"
+	# fh - the file object
+	# > to overwrite $symbol
+	# >> to append to symbol
+	#open(my $fh, '>', $symbol) or die "Could not open file '$symbol' $!";
+	#print $fh "$p";
+	#close $fh;
+        print "done\n";
+    }
+}
+
+
+SimpleServer->run(port => 50000);
+
+        
+
