e14fd4da79065e3d22b09a976be182def25898a2
dustin <dustin.cook@mines.sdsmt.edu>
2018-11-19 19:04:00 -0700

    we should be using socket instead of LWP i feel, this will make the client side easier to code, keeping it as branch for now

--- server/server.pl
+++ server/server.pl
@@ -11,25 +11,49 @@ use lib dirname(dirname abs_path $0) . '/server';
 use 5.010;
 
 use QueryManager;
-use LWP::Simple;
-use base qw(Net::Server);
+use IO::Socket::INET;
 
+# auto-flush on socket
+$| = 1;
 
-sub process_request
+my $qm = new QueryManager();
+ 
+# creating a listening socket
+my $socket = new IO::Socket::INET (
+    LocalHost => '0.0.0.0',
+    LocalPort => '50000',
+    Proto => 'tcp',
+    Listen => 5,
+    Reuse => 1
+);
+die "cannot create socket $!\n" unless $socket;
+print "server waiting for client connection on port 50000\n";
+
+while(1)
 {
-	my $qm = new QueryManager();
-
-	while (<STDIN>) 
-	{    
-        	s/[\r\n]+$//;   
-        	sleep 5; # dont get black listed
-
-		my $command = $_; # get command
-		$qm->dispatch($command);
-
-		print "done\n";
-	}
+    # waiting for a new client connection
+    my $client_socket = $socket->accept();
+ 
+    # get information about a newly connected client
+    my $client_address = $client_socket->peerhost();
+    my $client_port = $client_socket->peerport();
+    print "connection from $client_address:$client_port\n";
+ 
+    # read up to 1024 characters from the connected client
+    my $command = "";
+    $client_socket->recv($command, 1024);
+    print "received data: $command\n";
+    
+    # process the request
+    $respond = $qm->dispatch($command);
+ 
+    # write response data to the connected client
+    $client_socket->send($command);
+ 
+    # notify client that response has been sent
+    shutdown($client_socket, 1);
 }
+ 
+$socket->close();
 
 
-SimpleServer->run(port => 50000);
